2f849a74321e406cff1dc74c32b9d0d2cc80f6aa
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-13 16:18:31 +0530

    Refactor: statementSolvingUsing

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,20 +2,22 @@ echo "Welcome to Employee Wage Computation Program on Master Branch"
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
-randomCheck=$((RANDOM%3));
+empCheck=$((RANDOM%3));
 
-if [ $isFullTime -eq $randomCheck ]
-then
-        empHrs=8;
-echo "empHrs="$empHrs
-elif [ $isPartTime -eq $randomCheck ]
-then
-         empHrs=4;
-  echo "empHrs="$empHrs
-else
-        empHrs=0;
-       echo "empHrs="$empHrs
-fi
+case $empCheck in
+      $isFullTime)
+              empHrs=8
+echo "empHrs ="$empHrs
+               ;;
+      $isPartTime)
+              empHrs=4
+echo "empHrs ="$empHrs
+               ;;
+      *)
+        empHrs=0
+echo "empHrs ="$empHrs
+               ;;
+esac
 
 salary=$(($empHrs * $empRatePerHr));
 echo "salary="$salary
